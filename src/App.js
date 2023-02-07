a98503a52e9041c0244786324d80aa743d68e853
techstackmedia <31328325+techstackmedia@users.noreply.github.com>
2023-02-06 18:00:01 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route>
-            <Route path="/comingsoon" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<About color="red" />} />
             <Route path="/github" element={<Github />} />
           </Route>
